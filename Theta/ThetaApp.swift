0e91034cd46052d4957272d8714828e0d9de8c94
Jacob Parker <jacobparker1992@gmail.com>
2024-01-02 19:57:45 +0000

    Initial Commit

---
+++ Theta/ThetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThetaApp.swift
+//  Theta
+//
+//  Created by Jacob Parker on 02/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ThetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
